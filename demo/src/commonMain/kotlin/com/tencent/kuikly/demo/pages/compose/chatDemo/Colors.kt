9d07828215a0e6087bc1257e3c87c5c153985efb
ruifanyuan <ruifanyuan@tencent.com>
2025-08-06 21:06:28 +0800

    chore: temporarily disable ai demo

--- demo/src/commonMain/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/Colors.kt
+++ demo/src/commonMain/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/Colors.kt
@@ -1,44 +1,44 @@
-package com.tencent.kuikly.demo.pages.compose.chatDemo
-
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.staticCompositionLocalOf
-import com.tencent.kuikly.compose.ui.graphics.Color
-
-internal val LocalColorScheme = staticCompositionLocalOf { LightColoScheme }
-
-@Immutable
-internal class ColorScheme(
-
-    // 字体颜色
-    val t1: Color,
-    val t2: Color,
-    val t3: Color,
-    val aigcPrompt: Color,
-    val bgBlock: Color,
-    val newBgBlock: Color,
-    // 分割线
-    val lineFine: Color,
-    val lineStroke: Color,
-)
-
-internal val LightColoScheme = ColorScheme(
-    t1 = Color(0xFF333333),
-    t2 = Color(0xFF5C5C5C),
-    t3 = Color(0xFF999999),
-    aigcPrompt = Color(0xFF505DE5),
-    bgBlock = Color(0xFFF5F5F5),
-    newBgBlock = Color(0xFFF7F7F7),
-    lineFine = Color(0XFFF0F0F0),
-    lineStroke = Color(0XFFE6E6E6),
-)
-
-internal val DarkColorScheme = ColorScheme(
-    t1 = Color(0xFFD9D9D9),
-    t2 = Color(0xFFA9A9A9),
-    t3 = Color(0xFF696969),
-    aigcPrompt = Color(0xFF7780D9),
-    bgBlock = Color(0xFF262626),
-    newBgBlock = Color(0xFF262626),
-    lineFine = Color(0xFF292929),
-    lineStroke = Color(0xFF303030),
-)
+//package com.tencent.kuikly.demo.pages.compose.chatDemo
+//
+//import androidx.compose.runtime.Immutable
+//import androidx.compose.runtime.staticCompositionLocalOf
+//import com.tencent.kuikly.compose.ui.graphics.Color
+//
+//internal val LocalColorScheme = staticCompositionLocalOf { LightColoScheme }
+//
+//@Immutable
+//internal class ColorScheme(
+//
+//    // 字体颜色
+//    val t1: Color,
+//    val t2: Color,
+//    val t3: Color,
+//    val aigcPrompt: Color,
+//    val bgBlock: Color,
+//    val newBgBlock: Color,
+//    // 分割线
+//    val lineFine: Color,
+//    val lineStroke: Color,
+//)
+//
+//internal val LightColoScheme = ColorScheme(
+//    t1 = Color(0xFF333333),
+//    t2 = Color(0xFF5C5C5C),
+//    t3 = Color(0xFF999999),
+//    aigcPrompt = Color(0xFF505DE5),
+//    bgBlock = Color(0xFFF5F5F5),
+//    newBgBlock = Color(0xFFF7F7F7),
+//    lineFine = Color(0XFFF0F0F0),
+//    lineStroke = Color(0XFFE6E6E6),
+//)
+//
+//internal val DarkColorScheme = ColorScheme(
+//    t1 = Color(0xFFD9D9D9),
+//    t2 = Color(0xFFA9A9A9),
+//    t3 = Color(0xFF696969),
+//    aigcPrompt = Color(0xFF7780D9),
+//    bgBlock = Color(0xFF262626),
+//    newBgBlock = Color(0xFF262626),
+//    lineFine = Color(0xFF292929),
+//    lineStroke = Color(0xFF303030),
+//)
--- demo/src/commonMain/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/MarkdownTypography.kt
+++ demo/src/commonMain/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/MarkdownTypography.kt
@@ -1,114 +1,114 @@
-package com.tencent.kuikly.demo.pages.compose.chatDemo
-
-import androidx.compose.runtime.Composable
-import com.tencent.kuikly.compose.ui.text.TextLinkStyles
-import com.tencent.kuikly.compose.ui.text.TextStyle
-import com.tencent.kuikly.compose.ui.text.font.FontWeight
-import com.tencent.kuikly.compose.ui.unit.sp
-import com.tencent.kuiklybase.markdown.model.DefaultMarkdownTypography
-import com.tencent.kuiklybase.markdown.model.MarkdownTypography
-
-@Composable
-internal fun markdownTypography(
-    h1: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 20.sp,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 30.sp,
-    ),
-    h2: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 19.sp,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 28.sp,
-    ),
-    h3: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 19.sp,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 28.sp,
-    ),
-    h4: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 18.sp,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 27.sp,
-    ),
-    h5: TextStyle = h4,
-    h6: TextStyle = h4,
-
-    text: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 16.sp,
-        lineHeight = 28.sp
-    ),
-
-    code: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 14.sp,
-        lineHeight = 22.sp
-    ),
-
-    inlineCode: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 14.sp,
-        lineHeight = 28.sp
-    ),
-
-    quote: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t3,
-        fontSize = 16.sp,
-        lineHeight = 28.sp
-    ),
-
-    paragraph: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 16.sp,
-        lineHeight = 28.sp
-    ),
-
-    ordered: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 16.sp,
-        lineHeight = 28.sp
-    ),
-    bullet: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 16.sp,
-        lineHeight = 28.sp
-    ),
-    list: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.t1,
-        fontSize = 16.sp,
-        lineHeight = 28.sp
-    ),
-
-    link: TextStyle = TextStyle(
-        color = MdTheme.colorScheme.aigcPrompt,
-        fontSize = 16.sp,
-    ),
-    textLink: TextLinkStyles = TextLinkStyles(
-        style = link.toSpanStyle()
-    ),
-
-    table: TextStyle = text
-
-): MarkdownTypography = DefaultMarkdownTypography(
-    h1 = h1,
-    h2 = h2,
-    h3 = h3,
-    h4 = h4,
-    h5 = h5,
-    h6 = h6,
-    text = text,
-    quote = quote,
-    code = code,
-    inlineCode = inlineCode,
-    paragraph = paragraph,
-    ordered = ordered,
-    bullet = bullet,
-    list = list,
-    link = link,
-    textLink = textLink,
-    table = table,
-)
+//package com.tencent.kuikly.demo.pages.compose.chatDemo
+//
+//import androidx.compose.runtime.Composable
+//import com.tencent.kuikly.compose.ui.text.TextLinkStyles
+//import com.tencent.kuikly.compose.ui.text.TextStyle
+//import com.tencent.kuikly.compose.ui.text.font.FontWeight
+//import com.tencent.kuikly.compose.ui.unit.sp
+//import com.tencent.kuiklybase.markdown.model.DefaultMarkdownTypography
+//import com.tencent.kuiklybase.markdown.model.MarkdownTypography
+//
+//@Composable
+//internal fun markdownTypography(
+//    h1: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 20.sp,
+//        fontWeight = FontWeight.Bold,
+//        lineHeight = 30.sp,
+//    ),
+//    h2: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 19.sp,
+//        fontWeight = FontWeight.Bold,
+//        lineHeight = 28.sp,
+//    ),
+//    h3: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 19.sp,
+//        fontWeight = FontWeight.Bold,
+//        lineHeight = 28.sp,
+//    ),
+//    h4: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 18.sp,
+//        fontWeight = FontWeight.Bold,
+//        lineHeight = 27.sp,
+//    ),
+//    h5: TextStyle = h4,
+//    h6: TextStyle = h4,
+//
+//    text: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 16.sp,
+//        lineHeight = 28.sp
+//    ),
+//
+//    code: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 14.sp,
+//        lineHeight = 22.sp
+//    ),
+//
+//    inlineCode: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 14.sp,
+//        lineHeight = 28.sp
+//    ),
+//
+//    quote: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t3,
+//        fontSize = 16.sp,
+//        lineHeight = 28.sp
+//    ),
+//
+//    paragraph: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 16.sp,
+//        lineHeight = 28.sp
+//    ),
+//
+//    ordered: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 16.sp,
+//        lineHeight = 28.sp
+//    ),
+//    bullet: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 16.sp,
+//        lineHeight = 28.sp
+//    ),
+//    list: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.t1,
+//        fontSize = 16.sp,
+//        lineHeight = 28.sp
+//    ),
+//
+//    link: TextStyle = TextStyle(
+//        color = MdTheme.colorScheme.aigcPrompt,
+//        fontSize = 16.sp,
+//    ),
+//    textLink: TextLinkStyles = TextLinkStyles(
+//        style = link.toSpanStyle()
+//    ),
+//
+//    table: TextStyle = text
+//
+//): MarkdownTypography = DefaultMarkdownTypography(
+//    h1 = h1,
+//    h2 = h2,
+//    h3 = h3,
+//    h4 = h4,
+//    h5 = h5,
+//    h6 = h6,
+//    text = text,
+//    quote = quote,
+//    code = code,
+//    inlineCode = inlineCode,
+//    paragraph = paragraph,
+//    ordered = ordered,
+//    bullet = bullet,
+//    list = list,
+//    link = link,
+//    textLink = textLink,
+//    table = table,
+//)
