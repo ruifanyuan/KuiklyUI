8fb714601a7bc9b4bdd945de6e51c3542f8e84ce
ruifanyuan <ruifanyuan@tencent.com>
2025-08-06 21:33:54 +0800

    fix: compile issue

--- demo/src/androidMain/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/ChatDemo.android.kt
+++ demo/src/androidMain/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/ChatDemo.android.kt
@@ -1,11 +1,11 @@
-package com.tencent.kuikly.demo.pages.compose.chatDemo
-
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-
-actual fun getPlatform(): String = "Android"
-
-internal actual object NetworkClient {
-    actual val client: Any?
-        get() = HttpClient(OkHttp)
-}
+//package com.tencent.kuikly.demo.pages.compose.chatDemo
+//
+//import io.ktor.client.HttpClient
+//import io.ktor.client.engine.okhttp.OkHttp
+//
+//actual fun getPlatform(): String = "Android"
+//
+//internal actual object NetworkClient {
+//    actual val client: Any?
+//        get() = HttpClient(OkHttp)
+//}
--- demo/src/ohosArm64Main/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/ChatDemo.ohos.kt
+++ demo/src/ohosArm64Main/kotlin/com/tencent/kuikly/demo/pages/compose/chatDemo/ChatDemo.ohos.kt
@@ -1,8 +1,8 @@
-package com.tencent.kuikly.demo.pages.compose.chatDemo
-
-internal actual fun getPlatform(): String = "ohos"
-
-internal actual object NetworkClient {
-    actual val client: Any?
-        get() = null
-}
+//package com.tencent.kuikly.demo.pages.compose.chatDemo
+//
+//internal actual fun getPlatform(): String = "ohos"
+//
+//internal actual object NetworkClient {
+//    actual val client: Any?
+//        get() = null
+//}
