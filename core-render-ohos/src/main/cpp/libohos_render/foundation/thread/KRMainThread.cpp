4210c8f70b3629b2bf33a81359c4c83ea3fac8b1
ruifanyuan <ruifanyuan@tencent.com>
2025-09-03 20:42:33 +0800

    feat: use uv loop for task scheduling on main thread

--- core-render-ohos/src/main/cpp/libohos_render/foundation/thread/KRMainThread.cpp
+++ core-render-ohos/src/main/cpp/libohos_render/foundation/thread/KRMainThread.cpp
@@ -13,37 +13,132 @@
  * limitations under the License.
  */
 
-#include "KRMainThread.h"
 
+#include <cassert>
 #include <hilog/log.h>
+#include <unistd.h>
+#include <uv.h>
 #include "KRDelayThread.h"
+#include "libohos_render/utils/KRScopedSpinLock.h"
+#include "libohos_render/foundation/thread/KRMainThread.h"
 
+struct KRRunLoop;
+static struct KRRunLoop *g_main_loop_ = nullptr;
 
-napi_threadsafe_function g_threadsafe_func_handle = NULL;
-// 延时Api
-static void DispatchAsync(std::function<void()> task, int delayMilliseconds = 0) {
-    static KRDelayThread *gDelayThread = new KRDelayThread("kuiklyasync");
-    gDelayThread->DispatchAsync(task, delayMilliseconds);
-}
-class MainThreadTask {
- public:
-    explicit MainThreadTask(std::function<void()> func) : func(std::move(func)) {}
-
-    void execute() {
-        func();
+struct KRTimerTask final{
+public:
+    KRTimerTask(uv_loop_t* loop, std::function<void()> task, int timeoutMS): task_(task), timeoutMS_(timeoutMS){
+        uv_timer_init(loop, &timer_);
+        uv_handle_set_data((uv_handle_t*)&timer_, (void*)this);
     }
 
- private:
-    std::function<void()> func;
-};
-
-static void gcd_threadsafe_func(napi_env env, napi_value js_fun, void *context, void *data) {
-    MainThreadTask *task = static_cast<MainThreadTask *>(data);
-    if (task != nullptr) {
-        task->execute();
+    void Schedule(){
+        uv_timer_start(&timer_, &cb, timeoutMS_, INT_MAX);
+    }
+    void Close(){
+        uv_timer_stop(&timer_);
+        uv_close((uv_handle_t*)&timer_, nullptr);
+    }
+    static void cb(uv_timer_t* handle){
+        KRTimerTask* task = (KRTimerTask*)uv_handle_get_data((uv_handle_t*)handle);
+        task->Close();
+        task->task_();
         delete task;
     }
-}
+    uv_timer_t timer_;
+    std::function<void()> task_;
+    int timeoutMS_;
+};
+
+struct KRRunLoop{
+public:
+    static void InitMainLoop(napi_env env){
+        if (g_main_loop_){
+            return;
+        }
+        
+        struct uv_loop_s *uv_loop;
+        napi_get_uv_event_loop(env, &uv_loop);
+        struct KRRunLoop *loop = new KRRunLoop(uv_loop);
+        g_main_loop_ = loop;
+    }
+    
+    static struct KRRunLoop *MainLoop(){
+        return g_main_loop_;
+    }
+    
+    KRRunLoop(struct  uv_loop_s *loop): loop_(loop){
+        uv_async_init(loop, &async_, &cb);
+        uv_handle_set_data((uv_handle_t*)&async_, this);
+        tid_ = gettid();
+    }
+    
+    void Schedule(std::function<void()> task, int delayMS){
+         if (delayMS > 0){
+            ScheduleTimer(task, delayMS);
+         }else{
+            ScheduleAsyncTask(task);
+         }
+    }
+private:
+    void ScheduleAsyncTask(std::function<void()> task){
+        AppendTask(task);
+        uv_async_send(&async_);
+    }
+    void ScheduleTimer(std::function<void()> task, int delayMS){
+        if(gettid() == tid_){
+            // same thread as the loop
+            ScheduleTimerCurrentLoop(task, delayMS);
+        }else{
+            // different thread, dispatch to the same thread
+            ScheduleAsyncTask([this, task, delayMS](){
+                ScheduleTimerCurrentLoop(task, delayMS);
+            });
+        }
+    }
+    void ScheduleTimerCurrentLoop(std::function<void()> task, int delayMS){
+        assert(gettid() == tid_);
+        KRTimerTask *timerTask = new KRTimerTask(loop_, task, delayMS);
+        timerTask->Schedule();
+    }
+    bool ProcessNextTask(){
+        if(auto task = GetTask()){
+            task();
+            return true;
+        }
+        return false;
+    }
+    void AppendTask(std::function<void()> task){
+        KRScopedSpinLock lock(&tasks_lock_);
+        tasks_.push(task);
+    }
+    std::function<void()> GetTask(){
+        KRScopedSpinLock lock(&tasks_lock_);
+        if (tasks_.empty()){
+            return nullptr;
+        }
+        auto task = tasks_.front();
+        tasks_.pop();
+        return task;
+    }
+    
+    static void cb(uv_async_t* handle){
+        KRRunLoop *loop = (KRRunLoop*)uv_handle_get_data((uv_handle_t*)handle);
+        for(;;){
+            bool hasMore = loop->ProcessNextTask();
+            // strategy: process all and then exit
+            if(!hasMore){
+                break;
+            }
+        }
+    }
+    
+    pid_t tid_;
+    struct uv_loop_s *loop_;
+    uv_async_t async_;
+    std::queue<std::function<void()>> tasks_;
+    KRSpinLock tasks_lock_;
+};
 
 KRMainThread::KRMainThread() {
     // 在这里添加构造函数代码
@@ -53,67 +148,14 @@ void KRMainThread::Export(napi_env env, napi_value exports) {
     if ((nullptr == env) || (nullptr == exports)) {
         return;
     }
-    if (g_threadsafe_func_handle == NULL) {  // 注册安全函数
-        napi_value work_name;
-        napi_create_string_utf8(env, "Node-API Thread-safe Call from Async Work Item", NAPI_AUTO_LENGTH, &work_name);
-        napi_status status = napi_create_threadsafe_function(env, NULL, NULL, work_name, 0, 1, NULL, NULL, NULL,
-                                                             gcd_threadsafe_func, &g_threadsafe_func_handle);
-        if (status != napi_ok) {
-            napi_throw_error(env, "-1", "napi_create_threadsafe_function error");
-            return;
-        }
-    }
+
+    KRRunLoop::InitMainLoop(env);
 }
 
 void KRMainThread::RunOnMainThread(const std::function<void()> &task, int delayMilliseconds) {
-    if (delayMilliseconds > 0) {
-        DispatchAsync([task] { RunOnMainThread(task); }, delayMilliseconds);
-    } else {
-        if (g_threadsafe_func_handle) {
-            MainThreadTask *mainTask = new MainThreadTask(task);
-            napi_call_threadsafe_function(g_threadsafe_func_handle, static_cast<void *>(mainTask), napi_tsfn_blocking);
-        } else {
-            OH_LOG_Print(LOG_APP, LOG_ERROR, 0x7, "KRMainThread", "function handle is null");
-        }
-    }
-}
-
-static std::vector<std::function<void()>> &NextRunLoopTasks(bool isClear, const std::function<void()> &task) {
-    static std::vector<std::function<void()>> gTasks;
-    if (task) {
-        gTasks.push_back(task);
-    }
-    if (isClear) {
-        gTasks.clear();
-    }
-    return gTasks;
-}
-
-static bool NeedNextRunLoop(bool isGet, bool isSet, bool setValue) {
-    static bool gSetNeedNextRunloop = false;
-    if (isGet) {
-        return gSetNeedNextRunloop;
-    }
-    if (isSet) {
-        gSetNeedNextRunloop = setValue;
-    }
-    return gSetNeedNextRunloop;
+    KRRunLoop::MainLoop()->Schedule(task, delayMilliseconds);
 }
 
 void KRMainThread::RunOnMainThreadForNextLoop(const std::function<void()> &task) {
-    NextRunLoopTasks(false, task);
-    if (!NeedNextRunLoop(true, false, false)) {
-        NeedNextRunLoop(false, true, true);
-        DispatchAsync([] {
-            RunOnMainThread([] {
-                NeedNextRunLoop(false, true, false);
-                auto tasks = NextRunLoopTasks(false, nullptr);
-                int size = tasks.size();
-                for (auto &task : tasks) {
-                    task();
-                }
-                NextRunLoopTasks(true, nullptr);
-            });
-        });
-    }
+    KRRunLoop::MainLoop()->Schedule(task, 0);
 }
